2bcab57c0f271b05562d8743aa17216a8aa39627
Anton Plauche <anton@pivotdesign.com>
2024-05-17 14:39:06 -0500

    conversion of basic css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem"
+    },
     extend: {},
   },
   plugins: [
+    require("@tailwindcss/typography"),
     require('daisyui'),
   ],
   daisyui: {
